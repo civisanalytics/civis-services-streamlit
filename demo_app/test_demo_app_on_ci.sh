ddde0228835d83c9c080ddf7cf6a3a0fbd1247d0
jacksonlee-civis <jacksonlee@civisanalytics.com>
2024-08-22 18:07:09 -0400

    NH switch to uv in entrypoint.sh

--- demo_app/test_demo_app_on_ci.sh
+++ demo_app/test_demo_app_on_ci.sh
@@ -4,8 +4,8 @@
 
 set -e
 
-pip install --progress-bar off -r /app/demo_app/requirements.txt && \
-pip list -v && \
+uv pip install --no-progress --no-cache -r /app/demo_app/requirements.txt && \
+uv pip list -v && \
 python -u << HERE
 from streamlit.testing.v1 import AppTest
 app = AppTest.from_file("/app/demo_app/app.py", default_timeout=10)
